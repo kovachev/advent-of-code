297c8bb3f15dddf811a9d63205ac49ab7aad1835
Stefan Kovachev <st.kovachev@gmail.com>
2024-08-29 22:08:57 +0300

    aoc 2015 02 part 1 and 2

---
+++ 2015/aoc_2015_02/Box.cs
@@ -0,0 +1,24 @@
+﻿namespace aoc_2015_02;
+
+internal class Box
+{
+    private readonly int _length;
+    private readonly int _width;
+    private readonly int _height;
+    
+    internal Box(string dimensions)
+    {
+        var parts = dimensions.Split('x');
+        _length = int.Parse(parts[0]);
+        _width = int.Parse(parts[1]);
+        _height = int.Parse(parts[2]);
+    }
+    
+    internal int SurfaceArea => 2 * _length * _width + 2 * _width * _height + 2 * _height * _length;
+    
+    internal int Volume => _length * _width * _height;
+    
+    internal int SmallestSideArea => Math.Min(_length * _width, Math.Min(_width * _height, _height * _length));
+    
+    internal int SmallestCircumference => 2 * Math.Min(_length + _width, Math.Min(_width + _height, _height + _length));
+}
---
+++ 2015/aoc_2015_02/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace aoc_2015_02;
+
+internal class Program
+{
+    internal static void Main()
+    {
+        Console.WriteLine("Advent of Code 2015 - 02");
+        
+        var input = File.ReadAllLines("input.txt");
+        
+        var totalPaper = 0;
+        var totalRibbon = 0;
+        
+        foreach (var line in input)
+        {
+            var box = new Box(line);
+            totalPaper += box.SurfaceArea + box.SmallestSideArea;
+            totalRibbon += box.SmallestCircumference + box.Volume;
+        }
+        
+        Console.WriteLine($"Total paper needed: {totalPaper}");
+        Console.WriteLine($"Total ribbon needed: {totalRibbon}");
+    }
+}
