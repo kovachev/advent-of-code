53b98ed37f4719a2b4e1d49dc88a03ffe9257bf2
Stefan Kovachev <st.kovachev@gmail.com>
2024-08-30 22:38:20 +0300

    aoc 2015 08 part 1 and part 2

---
+++ 2015/aoc_2015_08/EscapedValue.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.RegularExpressions;
+
+namespace aoc_2015_08;
+
+internal class EscapedValue
+{
+    public int Length => _value.Length;
+
+    public int DecodedCharacters { get; private set; }
+
+    public int DoubleEncodedCharacters { get; private set; }
+
+    private readonly string _value;
+
+    internal EscapedValue(string value)
+    {
+        _value = value;
+
+        CountDecodedCharacters();
+        CountDoubleEncodedCharacters();
+    }
+
+    private void CountDecodedCharacters()
+    {
+        var value = _value[1..^1];
+
+        value = value.Replace("\\\"", "\"");
+
+        value = value.Replace("\\\\", "\\");
+
+        value = Regex.Replace(value, @"\\x[0-9a-f]{2}", "X", RegexOptions.Compiled); // just replace with a placeholder
+                                                                                     // we only need to count the characters
+
+        DecodedCharacters = value.Length;
+    }
+
+    private void CountDoubleEncodedCharacters()
+    {
+        var value = _value;
+
+        value = Regex.Replace(value, @"\\x[0-9a-f]{2}", "XXXXX", RegexOptions.Compiled); // just replace with a placeholder
+                                                                                         // we only need to count the characters
+
+        value = value.Replace("\\", "\\\\");
+
+        value = value.Replace("\"", "\\\"");
+
+        DoubleEncodedCharacters = value.Length + 2; // +2 for the surrounding quotes
+    }
+}
---
+++ 2015/aoc_2015_08/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace aoc_2015_08;
+
+internal class Program
+{
+    internal static void Main()
+    {
+        Console.WriteLine("Advent of Code 2015 - 08");
+        
+        var input = File.ReadAllLines("input.txt");
+        
+        var entries = input.Select(x => new EscapedValue(x))
+                           .ToArray();
+        
+        var totalCharacters = entries.Sum(x => x.DecodedCharacters);
+        var totalDoubleEncodedCharacters = entries.Sum(x => x.DoubleEncodedCharacters);
+        var totalLength = entries.Sum(x => x.Length);
+        
+        Console.WriteLine($"Part 1: {totalLength - totalCharacters}");
+        Console.WriteLine($"Part 2: {totalDoubleEncodedCharacters - totalLength}");
+    }
+}
