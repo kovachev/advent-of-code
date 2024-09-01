9c95a6a005e9ad7b48ae1ac89cf2fc548d797038
Stefan Kovachev <st.kovachev@gmail.com>
2024-09-01 21:55:32 +0300

    aoc 2015 day 11 both parts and day 25

---
+++ 2015/aoc_2015_11/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace aoc_2015_11;
+
+internal class Program
+{
+    internal static void Main(string[] args)
+    {
+        Console.WriteLine("Advent of Code 2015 - 11");
+        
+        // Part 1
+        var input = "vzbxkghb".ToCharArray();
+
+        input = NextPassword(input);
+       
+        Console.WriteLine("Part 1: The next valid password is " + new string(input));
+        
+        // Part 2
+        input = NextPassword(input);
+        
+        Console.WriteLine("Part 2: The next valid password is " + new string(input));
+    }
+
+    private static char[] NextPassword(char[] input)
+    {
+        var count = 0;
+        var found = false;
+        while (count++ < 1000000)
+        {
+            input = IncrementPassword(input);
+            if (IsValidPassword(input))
+            {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            throw new Exception("No valid password found");
+        }
+        
+        return input;
+    }
+    
+    private static char[] IncrementPassword(char[] password)
+    {
+        for (var i = password.Length - 1; i >= 0; i--)
+        {
+            if (password[i] == 'z')
+            {
+                password[i] = 'a';
+            }
+            else
+            {
+                password[i]++;
+                break;
+            }
+        }
+        
+        return password;
+    }
+    
+    private static bool IsValidPassword(char[] password)
+    {
+        var hasStraight = false;
+        
+        for (var i = 0; i < password.Length; i++)
+        {
+            if (password[i] == 'i' || 
+                password[i] == 'o' || 
+                password[i] == 'l')
+            {
+                return false;
+            }
+            
+            if (i < password.Length - 2)
+            {
+                if (password[i] == password[i + 1] - 1 &&
+                    password[i] == password[i + 2] - 2)
+                {
+                    hasStraight = true;
+                }
+            }
+        }
+        
+        var pairCount = 0;
+        for (var i = 0; i < password.Length - 1; i++)
+        {
+            if (password[i] == password[i + 1])
+            {
+                pairCount++;
+                i++;
+            }
+        }
+        
+        return hasStraight && pairCount >= 2;
+    }
+}
---
+++ 2015/aoc_2015_25/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace aoc_2015_25;
+
+internal class Program
+{
+    internal static void Main(string[] args)
+    {
+        Console.WriteLine("Advent of Code 2015 - 25");
+        
+        // Part 1
+        // To continue, please consult the code grid in the manual. Enter the code at row 2981, column 3075.
+        const long row = 2981;
+        const long column = 3075;
+        const long startValue = 20151125;
+        
+        // Calculate column value at row 1
+        var columnValueAtRow1 = 0;
+        for (var i = 1; i <= column; i++)
+        {
+            columnValueAtRow1 += i;
+        }
+        
+        Console.WriteLine("Column value at row 1: " + columnValueAtRow1);
+        
+        long iterations = columnValueAtRow1;
+        var increment = column;
+        for (var i = 0; i < row - 1; i++)
+        {
+            iterations += increment++;
+        }
+
+        iterations--; // We start at 1 as we already have the start value
+        
+        Console.WriteLine("Iterations needed to reach target value: " + iterations);
+        
+        var code = startValue;
+        for (var i = 0; i < iterations; i++)
+        {
+            code = NextCode(code);
+        }
+        
+        Console.WriteLine($"Part 1: The code at row {row}, column {column} is {code}");
+    }
+    
+    private static long NextCode(long previousCode)
+    {
+        return (previousCode * 252533) % 33554393;
+    }
+}
