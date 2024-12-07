9b72b8e855f9cb8f509bf0680433688128fedfde
skovachev <st.kovachev@gmail.com>
2024-12-07 11:41:25 +0200

    aoc day 6 part 1 and day 7

---
+++ 2024/aoc_2024_06/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace aoc_2024_06;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Advent of Code 2024 - Day 6");
+        
+        var input = File.ReadAllLines("input.txt");
+        //var input = File.ReadAllLines("sample.txt");
+
+        var map = new char[input[0].Length, input.Length];
+        
+        for (var y = 0; y < input.Length; y++)
+        {
+            for (var x = 0; x < input[y].Length; x++)
+            {
+                map[x,y] = input[y][x];
+            }
+        }
+        
+        var position = FindStartingPoint(map);
+        map[position.X, position.Y] = 'X';
+        
+        var offset = -1;
+        var horizontal = false;
+
+        var len = 1;
+        while (true)
+        {
+            var x = horizontal ? position.X + offset : position.X;
+            var y = horizontal ? position.Y : position.Y + offset;
+            
+            var next = new Position(x, y);
+            if (!IsOnMap(map, next))
+            {
+                break;
+            }
+                
+            if (map[next.X, next.Y] == '#')
+            {
+                offset = horizontal == false ? -offset : offset;
+                horizontal = !horizontal;
+            }
+            else
+            {
+                if (map[next.X, next.Y] == '.')
+                {
+                    map[next.X, next.Y] = 'X';
+                    len++;
+                }
+                
+                position = next;
+            }
+        }
+        
+        Console.WriteLine($"Part 1: {len}");
+    }
+    
+    private static bool IsOnMap(char[,] map, Position position)
+    {
+        return position.X >= 0 && position.X < map.GetLength(0) &&
+               position.Y >= 0 && position.Y < map.GetLength(1);
+    }
+    
+    private static Position FindStartingPoint(char[,] map)
+    {
+        for (var y = 0; y < map.GetLength(1); y++)
+        {
+            for (var x = 0; x < map.GetLength(0); x++)
+            {
+                if (map[x,y] == '^')
+                {
+                    return new Position(x, y);
+                }
+            }
+        }
+        
+        return new Position(-1, -1);
+    }
+}
+
+internal record Position(int X, int Y);
---
+++ 2024/aoc_2024_07/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace aoc_2024_07;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Advent of Code 2024 - Day 7");
+
+        //var input = File.ReadAllLines("sample.txt");
+        var input = File.ReadAllLines("input.txt");
+
+        var equations = new List<Equation>();
+
+        foreach (var line in input)
+        {
+            var split = line.Split(":");
+            var result = long.Parse(split[0]);
+            var values = split[1].Trim().Split(" ").Select(long.Parse).ToArray();
+            var equation = new Equation(result, values);
+            equations.Add(equation);
+        }
+
+        Console.WriteLine($"Part 1: {Solution(equations, part2: false)}");
+        Console.WriteLine($"Part 2: {Solution(equations, part2: true)}");
+    }
+
+    private static long Solution(IEnumerable<Equation> equations, bool part2)
+    {
+        var sum = 0L;
+
+        foreach (var equation in equations)
+        {
+            sum += SolveEquation(equation, part2);
+        }
+
+        return sum;
+    }
+
+    private static long SolveEquation(Equation equation, bool part2)
+    {
+        var values = equation.Values;
+        var result = equation.Result;
+
+        if (TrySolve(values, result, 0, values[0], part2, out var solution))
+        {
+            return solution;
+        }
+
+        return 0;
+    }
+
+    private static bool TrySolve(
+        long[] values, long result, int index, long current, bool part2,
+        out long solution)
+    {
+        if (index == values.Length - 1)
+        {
+            if (current == result)
+            {
+                solution = current;
+                return true;
+            }
+
+            solution = 0;
+            return false;
+        }
+
+        if (TrySolve(values, result, index + 1, current + values[index + 1], part2, out solution) ||
+            TrySolve(values, result, index + 1, current * values[index + 1], part2, out solution) ||
+            (part2 && TrySolve(values, result, index + 1, long.Parse($"{current}{values[index+1]}"), part2, out solution)))
+        {
+            return true;
+        }
+
+        solution = 0;
+        return false;
+    }
+}
+
+internal record Equation(long Result, long[] Values);
