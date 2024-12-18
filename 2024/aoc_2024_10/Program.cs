b4c8b62d5d0e4b41fbfd116797e442e823be63d3
Stefan Kovachev <st.kovachev@gmail.com>
2024-12-18 19:30:44 +0200

    refactor day 10

--- 2024/aoc_2024_10/Program.cs
+++ 2024/aoc_2024_10/Program.cs
@@ -1,28 +1,21 @@
-﻿namespace aoc_2024_10;
+﻿using Helpers;
+
+namespace aoc_2024_10;
 
 internal class Program
 {
+    private const char StartMarker = '0';
+    
     private static void Main()
     {
         Console.WriteLine("Advent of Code 2024 - Day 10");
         
-        var input = File.ReadAllLines("input.txt");
-        //var input = File.ReadAllLines("sample.txt");
+        var map = new Map("input.txt");
+        //var map = new Map("sample.txt");
 
-        var map = input.Select(x => x.ToCharArray()).ToArray();
-
-        var startingPositions = new List<Position>();
-        
-        for (var y = 0; y < map.Length; y++)
-        {
-            for (var x = 0; x < map[y].Length; x++)
-            {
-                if (map[y][x] == '0')
-                {
-                    startingPositions.Add(new Position(x, y));
-                }
-            }
-        }
+        var startingPositions = map.Where(x => x.Value == StartMarker)
+                                   .Select(x => x.Position)
+                                   .ToArray();
         
         var sumPart1 = 0;
         var sumPart2 = 0;
@@ -37,16 +30,11 @@ internal class Program
             {
                 var current = queue.Dequeue();
 
-                var currentValue = int.Parse(map[current.Y][current.X].ToString());
+                var currentValue = int.Parse(map[current].ToString());
                 
-                foreach (var neighbour in GetNeighbours(current))
+                foreach (var neighbour in map.GetNeighbours(current))
                 {
-                    if (!neighbour.IsOnMap(map))
-                    {
-                        continue;
-                    }
-
-                    var neighbourValue = int.Parse(map[neighbour.Y][neighbour.X].ToString());
+                    var neighbourValue = int.Parse(map[neighbour].ToString());
                     
                     var diff = neighbourValue - currentValue;
 
@@ -83,27 +71,5 @@ internal class Program
         
         Console.WriteLine($"Part 1: {sumPart1}");
         Console.WriteLine($"Part 2: {sumPart2}");
-    }
-    
-    private static IEnumerable<Position> GetNeighbours(Position position)
-    {
-        yield return position + new Position(0, 1);
-        yield return position + new Position(1, 0);
-        yield return position + new Position(0, -1);
-        yield return position + new Position(-1, 0);
-    }
-}
-
-internal record Position(int X, int Y, Position Parent = null)
-{
-    public static Position operator +(Position a, Position b) => new(a.X + b.X, a.Y + b.Y);
-    
-    public static Position operator -(Position a, Position b) => new(a.X - b.X, a.Y - b.Y);
-    
-    public bool IsOnMap(char[][] map) => X >= 0 && X < map[0].Length && Y >= 0 && Y < map.Length;
-
-    public override string ToString()
-    {
-        return $"({X}, {Y})";
     }
 }
